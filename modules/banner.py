128e7fbb17a7382178a88b5340b5c42935b7bcd1
charge <74613350+Phew@users.noreply.github.com>
2021-08-03 17:17:29 -0500

    Create banner.py

---
+++ modules/banner.py
@@ -0,0 +1,14 @@
+import colorama
+from colorama import *
+
+BANNER = f'''{Fore.MAGENTA}
+      {Fore.MAGENTA}___           __    ____        __ {Fore.CYAN} __            __  
+     {Fore.MAGENTA}/ _ )___ _____/ /__ / __/__  ___/ / {Fore.CYAN}/ /  ___ ___ _/ /__   {Fore.YELLOW} - github.com/Phew
+    {Fore.MAGENTA}/ _  / _ `/ __/  '_// _// _ \/ _  / {Fore.CYAN}/ /__/ -_) _ `/  '_/   {Fore.YELLOW} - github.com/BlackRabbit-0
+   {Fore.MAGENTA}/____/\_,_/\__/_/\_\/___/_//_/\_,_/ {Fore.CYAN}/____/\__/\_,_/_/\_\ 
+                                                            
+
+                    {Fore.MAGENTA} Oops gotcha {Fore.CYAN}backend{Fore.RESET}!
+                    
+                    
+    '''
